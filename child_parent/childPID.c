cc1f7b8e336921c528726ed62bd34a26646ddd3c
Imran4424 <akashganga.programming.534363@gmail.com>
2018-02-28 20:06:26 +0600

    parent child implementation

--- child_parent/childPID.c
+++ child_parent/childPID.c
@@ -1,10 +1,37 @@
+/*
+	Here we implement a code to create child proecess
+
+	here we create a single child process
+
+	here "fork()" function is called to create the child process
+
+	after calling fork(),
+
+						it will return the child proceess id to the parent process
+
+						it will return 0 to the child process
+
+	we use "getpid()" function to achieve current process id, for both parent and child process
+
+
+	we use one more function for child process
+	that is "getppid()"
+	to achieve parent process id for this child
+
+
+*/
+
+
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/wait.h>
 
-int main(){
+int main()
+{
 	//int i;
-	pid_t childPID, myPID;
+	pid_t childPID, myPID, parent_pid;
+
+	char message[100];
 	
 	/*
 	   0 > childPID --> unsuccessful
@@ -14,6 +41,10 @@ int main(){
 	
 	childPID = fork();
 	myPID = getpid();
+
+	
+
+	//sleep(1);
 	
 	if (childPID < 0)
 	{
@@ -21,12 +52,17 @@ int main(){
 	}
 	else if(childPID == 0 )
 	{
-		printf("I am child - %d,My parent is  %d \n\n", childPID, myPID);
+		parent_pid = getppid();
+
+		printf("I am child - %d,My Parent is %d \n\n",myPID,parent_pid);
 	}
 	else
 	{
 		printf("\nI am parent of child %d, My PID is %d .\n\n", childPID, myPID);
-		wait(NULL);
+
+
+		wait(NULL);     //here wait function return the pid of dead child
+
 		printf("My child is really dead.\n\n");
 	}
 
