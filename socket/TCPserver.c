adf7a74309e7ca809957a06b411403c7554ff9e1
Imran4424 <akashganga.programming.534363@gmail.com>
2018-05-04 15:59:28 +0600

    socket

--- socket/TCPserver.c
+++ socket/TCPserver.c
@@ -119,7 +119,10 @@ int main()
 
 			write = true;
 
-      		
+      if(strcmp(client_response,"good bye") == 0 || strcmp(client_response,"Good Bye") == 0 || strcmp(client_response,"Good bye") == 0)
+        {
+            good_bye_count++;
+        } 		
 
 		}
 
