4271053e8b9d436d4688b43d581373026ad86488
Imran4424 <akashganga.programming.534363@gmail.com>
2018-05-04 15:54:31 +0600

    socket

--- socket/Client.c
+++ socket/Client.c
@@ -109,6 +109,11 @@ int main() {
         {
             perror("sending failed");
         }
+
+        if(strcmp(client_message,"good bye") == 0 || strcmp(server_message,"Good Bye") == 0 || strcmp(server_message,"Good bye") == 0)
+        {
+            good_bye_count++;
+        }
     }
 
     
