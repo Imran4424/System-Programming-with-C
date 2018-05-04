4be2cbf1b67e15a7e95c971e84f096d0950f8adf
Imran4424 <akashganga.programming.534363@gmail.com>
2018-05-04 11:21:25 +0600

    system programming

--- socket/Server.c
+++ socket/Server.c
@@ -66,7 +66,7 @@ int main()
     int client_socket;
     client_socket = accept(server_socket, NULL, NULL);
 
-    // Send message to the client
+    // Send and receive message to the client
     
 
 	//declaring the variables
